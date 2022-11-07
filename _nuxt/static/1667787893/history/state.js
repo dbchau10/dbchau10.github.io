e341e65ba7e4f4c5ce49f0a38b5428ba97b49b03
Chau Dang <82007790+dbchau10@users.noreply.github.com>
2022-11-07 09:38:14 +0700

    Add files via upload

---
+++ _nuxt/static/1667787893/history/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdbchau10.github.io\u002F_nuxt\u002Fstatic\u002F1667787893",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhistory",config:{_app:{basePath:"\u002Fdbchau10.github.io\u002F",assetsPath:"\u002Fdbchau10.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
