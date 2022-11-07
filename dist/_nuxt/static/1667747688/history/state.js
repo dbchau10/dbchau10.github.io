523808f78942d15422296692db512144844c2374
dbchau10 <dbchau10@gmail.com>
2022-11-07 09:21:47 +0700

    a

--- dist/_nuxt/static/1667747688/history/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdbchau10.github.io\u002F_nuxt\u002Fstatic\u002F1667747688",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhistory",config:{_app:{basePath:"\u002Fdbchau10.github.io\u002F",assetsPath:"\u002Fdbchau10.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
